62503611a85f2e66f00b55f381d2cdda9262f9cf
rupel <23055682+rupel190@users.noreply.github.com>
2025-08-21 15:26:24 +0200

    add kanshi; add mpvpaper, not working yet

---
+++ dot_config/hypr/executable_mpvpaperfix.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Fix mpvpaper memory leak by restarting every 30min
+
+start_mpvpaper() {
+  # mpvpaper -s -o "no-audio loop hwdec=auto vf=crop=w=3440:h=1440 gpu-api=vulkan" DP-1 ~/dotty_config/wallpaper/wide &
+  # mpvpaper -s -o "no-audio loop hwdec=auto vf=crop=w=1080:h=1920 gpu-api=vulkan" HDMI-A-1 ~/dotty_config/wallpaper/vertical &
+  mpvpaper -s -o "no-audio loop hwdec=auto gpu-api=vulkan" ALL ~/Videos/wallpaper/garden-house-lofi-rainy-day-moewalls-com-ctp-macchiato.mp4 &
+
+}
+
+kill_mpvpaper() {
+  killall mpvpaper
+}
+
+while true; do
+  start_mpvpaper
+  sleep 1800
+  kill_mpvpaper
+  sleep 1
+done
